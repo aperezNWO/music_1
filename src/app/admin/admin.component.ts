e8bda9b994b411f0bd83719bbe59a35d8a46f463
alejandro.perez.acosta@hotmail.com <aperezNWO>
2024-02-10 22:02:50 -0500

    Add Songs. Pendiente - Delete Added song.

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AlbumsService } from '../albums.service';
-import { Album } from '../album.module';
+import { Album, ISong } from '../album.module';
 
 @Component({
   selector: 'app-admin',
@@ -9,41 +9,41 @@ import { Album } from '../album.module';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
-addSong() {
-throw new Error('Method not implemented.');
-}
-  albumForm!: FormGroup
-  albums: any;
-  loading: any;
-  musicAlbum: any;
+  //
+  albumForm! : FormGroup
+  //albums     : any;
+  //loading    : any;
+  //musicAlbum : any;
+  songArray    : ISong[] = [];
   //
   constructor(private albumService: AlbumsService) {
-
+      //
   }
   //
   ngOnInit(): void {
     this.createForm()
-
   }
-
-createForm() {
-  this.albumForm = new FormGroup({
-    title: new FormControl('',[Validators.required]),
-    artist: new FormControl('',[Validators.required]),
-    date: new FormControl('',[Validators.required]),
-    imageURL: new FormControl('',[Validators.required]),
-  });
-  
-
-
-}
-onSubmit() {
+  //
+  createForm() {
+    this.albumForm = new FormGroup({
+      title       : new FormControl('',[Validators.required]),
+      artist      : new FormControl('',[Validators.required]),
+      date        : new FormControl('',[Validators.required]),
+      imageURL    : new FormControl('',[Validators.required]),
+      songName    : new FormControl('',[Validators.required]),
+      songDuration: new FormControl('',[Validators.required]),
+    });
+  //
+  }
+  onSubmit() {
+    //
     console.log('form entry')
+    //
     let lastId     : number  = this.albumService.getLastId();
     let P_title    : string  = this.albumForm.value["title"]    || "";
     let P_artist   : string  = this.albumForm.value["artist"]   || "";      
     let P_date     : string  = this.albumForm.value["date"]     || "";
-    let P_imageURL : string  = this.albumForm.value["imageURL"] || "";      
+    let P_imageURL : string  = this.albumForm.value["imageURL"] || "";  
     //
     console.log("P_title:"    , P_title);
     console.log("P_artist:"   , P_artist);
@@ -55,11 +55,24 @@ onSubmit() {
       artist   : P_artist,
       imageUrl : P_imageURL,
       date     : [P_date],
-      songs    : []
+      songs    : this.songArray
     };
     //
-    //albumItem.artist = artist
-    //
     this.albumService.addAlbum(albumItem);
+  }
+  //
+  addSong() {
+    //
+    console.log("Adding song...");
+    //
+    let p_songName       : string  = this.albumForm.value["songName"]       || "";
+    let p_songDuration   : number  = this.albumForm.value["songDuration"]   || "";  
+    //
+    let song : ISong = {
+      title   : p_songName,
+      duration: p_songDuration
+    } 
+    //
+    this.songArray.push(song);
   }
 }
